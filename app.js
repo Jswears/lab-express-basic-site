66f4990975e16b37a5b7e1280a6bcdb0664c7e29
Jswears <ji.swearssalinas@gmail.com>
2023-07-03 14:09:31 +0200

    new commit

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
 
-//node app.js to run it once, or nodemon app.js that it keeps on running and updated automatically.
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-
 //makes publick available
 app.use(express.static("public"));
 
@@ -13,12 +8,23 @@ app.use(express.static("public"));
 app.get("/home", (request, response, next) =>
   response.sendFile(__dirname + "/views/home.html")
 );
+
+//about route
 app.get("/about", (request, response, next) =>
   response.sendFile(__dirname + "/views/about.html")
 );
+
+//work route
 app.get("/works", (request, response, next) =>
   response.sendFile(__dirname + "/views/works.html")
 );
+
+//gallery route
 app.get("/gallery", (request, response, next) =>
   response.sendFile(__dirname + "/views/gallery.html")
 );
+
+//node app.js to run it once, or nodemon app.js that it keeps on running and updated automatically.
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
